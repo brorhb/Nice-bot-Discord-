e84714e6ba670776b8c825ac05c97ee69a6c7f8a
bror <@@>
2020-05-29 15:15:11 +0200

    bloop

--- daddy.js
+++ daddy.js
@@ -1,7 +1,8 @@
 const daddyGifs = [
   'https://media.giphy.com/media/U1V98l64fqbLxBtFJZ/giphy.gif',
   'https://media.giphy.com/media/h6lbwqcLGeg47YA4A2/giphy.gif',
-  'https://media.giphy.com/media/lPFmR2vjyl07TXu1CA/giphy.gif'
+  'https://media.giphy.com/media/lPFmR2vjyl07TXu1CA/giphy.gif',
+  'https://media.giphy.com/media/l1J9rNo17YCabI6dO/giphy.gif'
 ]
 
 module.exports = daddyGifs
