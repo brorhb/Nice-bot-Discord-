59a28a81d501589efed5be113f7c8704910c52ec
bror <@@>
2020-06-16 14:26:29 +0200

    removed dead code

--- bot.js
+++ bot.js
@@ -9,9 +9,13 @@ const bot = new Discord.Client({
 
 bot.on('message', function (user, id, channel, msg, evt) {
   msg = msg.toLowerCase()
-  if ((msg.includes('nice') || msg.includes(' 69 ')) && id != bot.id) {
-    getJsonData(function (err, data) {
-      niceJSONHandler(err, data, user, id, channel, msg, evt)
+  if ((msg.includes('nice')) && id != bot.id) {
+    bot.sendMessage({
+      to: channel,
+      message: `
+A big fat NICE from *${user}*
+https://tenor.com/view/nice-south-park-not-bad-good-one-gif-4294992
+`
     })
   } else if (
     msg == '!daddy' ||
@@ -38,64 +42,5 @@ function daddyResponse(user, id, channel, msg, evt) {
   bot.sendMessage({
     to: channel,
     message: gif
-  })
-}
-
-function niceJSONHandler(err, data, user, id, channel, msg, evt) {
-  if (err) {
-    console.log(err)
-    return
-  }
-  if (!data) data = {}
-  if (data[user]) {
-    data[user] = data[user] + 1
-  } else {
-    data[user] = 1
-  }
-  bot.sendMessage({
-    to: channel,
-    message: `
-A big fat NICE from *${user}*
-https://tenor.com/view/nice-south-park-not-bad-good-one-gif-4294992
-`
-  })
-}
-
-function top3(data) {
-  let result = ''
-  const leaderboard = Object.keys(data).sort((a, b) => data[b] - data[a])
-  for (var i = 0; i < 3; i++) {
-    let user = leaderboard[i]
-    let score = data[leaderboard[i]] || false
-    if (user) {
-      result += `${user}: *${score}*\n`
-    }
-  }
-  return result
-}
-
-async function wirteData(data) {
-  data = JSON.stringify(data)
-  try {
-    await fs.writeFile('counter.json', data, function (err, data) {
-      if (err) throw err
-    })
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-async function getJsonData(cb) {
-  await fs.readFile('counter.json', 'utf8', function (err, data) {
-    if (err) {
-      cb(err, null)
-      return
-    } else {
-      try {
-        cb(null, JSON.parse(data))
-      } catch (err) {
-        cb(err, null)
-      }
-    }
   })
 }
